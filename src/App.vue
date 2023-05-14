fdd8b080023ea84fdb2e1698ec337f0622ecf4a4
pierre golfier <pgolfier.pro@gmail.com>
2023-05-15 00:16:02 +0200

    fix: seo lighthouse recommendations

--- src/App.vue
+++ src/App.vue
@@ -26,6 +26,10 @@ useHead({
   meta: [
     { charset: 'utf-8' },
     { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    {
+      name: 'description',
+      content: 'Planifiez, partagez et réalisez vos séances de HIIT',
+    },
   ],
 })
 
